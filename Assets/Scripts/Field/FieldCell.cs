67ba4163919261985c1d70f99ffdd73696785482
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2023-03-17 04:58:42 +0400

    BetaBuild

--- Assets/Scripts/Field/FieldCell.cs
+++ Assets/Scripts/Field/FieldCell.cs
@@ -18,8 +18,16 @@ public class FieldCell : MonoBehaviour
 
     public void Chop(){
         if(_plantHolder.IsGrown){
-            Instantiate(_dropPrefab,transform.position, Quaternion.identity);
+            Instantiate(_dropPrefab,transform.position + RandomVector(new Vector3(0.1f,0.05f,0.1f)), Quaternion.Euler(new Vector3(0,Random.Range(-15,15),0)));
             _plantHolder.Chop();
         }
+    }
+
+    Vector3 RandomVector(Vector3 range){
+        return new Vector3(
+            Random.Range(-range.x,range.x),
+            Random.Range(-range.y,range.y),
+            Random.Range(-range.z,range.z)
+        );
     }
 }
